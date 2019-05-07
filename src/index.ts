1864e1721ccb3cfa0a2b6809029064cae84ad8df
e_ntyo <entyo.contact@gmail.com>
2019-05-07 22:48:04 +0900

    debug

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { getNodeEnv } from './nodeenv';
 import { runAllAsync } from './program';
 
 const nodeEnv = getNodeEnv().run()
+console.log(process.env.NODE_ENV, process.env.TWINS_ID, process.env.TWINS_PASS, process.env.SENDGRID_API_KEY);
 if (nodeEnv === "development") {
   runAllAsync;
 } else {
